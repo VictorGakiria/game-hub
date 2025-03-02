e6c5e02cf24a21524b279a3170e2bf66d08c66db
Kaizen Mark <vmark119@gmail.com>
2025-03-02 23:17:56 +0300

    ll

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -4,10 +4,16 @@ import { Grid } from "@chakra-ui/react"
 export default function Grids(){
   return (
     <Grid templateColumns="repeat(4, 5fr)" gap="2" color={"orange"}
-     bg={"white"} p={50} borderRight={'1px'}>
-      <div style={{ border: '1px solid black' }}>20</div>
-      <div style={{ border: '1px solid black' }} >20</div>
-      <div style={{ border: '1px solid black' }}>20</div>
+     bg={"white"} p={50} >
+      <div style={{ border: '1px solid black', backgroundColor:'darkred' }}>20</div>
+      <div style={{ border: '1px solid black', backgroundColor:'darkgreen' }}>20</div>
+      <div style={{ border: '1px solid black' ,backgroundColor:'darkorange' }} >20</div>
+      <div style={{ border: '1px solid black' , backgroundColor:'darkgrey'}}>20</div>
+      <div style={{ border: '1px solid black', backgroundColor:'darkblue' }}>20</div>
+      <div style={{ border: '1px solid black', backgroundColor:'brown' }} >20</div>
+      <div style={{ border: '1px solid black', backgroundColor:'black'}}>20</div>
+      <div style={{ border: '1px solid black', backgroundColor:'green' }} >20</div>
+      <div style={{ border: '1px solid black', backgroundColor:'red' }}>20</div>
         </Grid>
   )
 }
